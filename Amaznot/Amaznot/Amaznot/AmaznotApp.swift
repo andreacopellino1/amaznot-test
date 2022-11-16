531c3f0a547251f9cac8821646882480713a126c
andreacopellino1 <113526032+andreacopellino1@users.noreply.github.com>
2022-11-16 09:36:14 +0100

    Add files via upload

---
+++ Amaznot/Amaznot/Amaznot/AmaznotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmaznotApp.swift
+//  Amaznot
+//
+//  Created by Andrea Copellino on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AmaznotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
